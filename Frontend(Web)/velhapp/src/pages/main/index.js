adef88e27a4153b9a9f6d8916e61a146a6f34788
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-05 22:15:26 -0300

    Main

--- Frontend(Web)/velhapp/src/pages/main/index.js
+++ Frontend(Web)/velhapp/src/pages/main/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import './style.css';
+import 
+{ 
+  Container,
+  Row,
+  Col,
+  Card,
+} from 'react-bootstrap';
 
 export default class Main extends Component {
   render() {
     return (
-        <div>
-          <h2>OI</h2>
-        </div>
+      <Container>
+        <Row>
+          <Col>
+            <Card className="shadow p-5 mb-5 bg-white rounded formLogin box">
+              <h1>OOOOIIIIII</h1>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
